4b3fb9b3de84a6fea5af79cf449e6e13e6e923ed
Christina Howard <clhoward33@icloud.com>
2020-09-11 05:20:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-create_an_array (element)<< "ice"
+array_element.push  "ice"
 end
 
 def add_element_to_start_of_array(array, element)
